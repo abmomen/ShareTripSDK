687ee958cc4883c786d7f7550cd4233e0ad1966f
Abdul Momen <abdulmomen130@gmail.com>
2023-01-19 16:22:27 +0600

    removes duplicate files

--- ShareTripSDK/Classes/STCoreKit/Extensions/Date/DateExtension.swift
+++ ShareTripSDK/Classes/STCoreKit/Extensions/Date/DateExtension.swift
@@ -781,3 +781,26 @@ public enum DateStyleType {
     /// Very short month: "F"
     case veryShortMonth
 }
+
+public extension Date {
+    static func validateDateOfBirth(_ dob: Date, tillDate: Date, of travellerType: TravellerType) -> Result<Void, AppError> {
+        guard let age = Calendar(identifier: .gregorian).dateComponents([.year], from: dob, to: tillDate).year else {
+            return .success(())
+        }
+        switch travellerType {
+        case .infant:
+            guard age < 2 else {
+                return .failure(.validationError("Infant's age must be \(travellerType.requiredInfo)"))
+            }
+        case .child:
+            guard 2 <= age && age < 12 else {
+                return .failure(.validationError("Child's age must be \(travellerType.requiredInfo)"))
+            }
+        case .adult:
+            guard age >= 12 else {
+                return .failure(.validationError("Adult must be at least \(travellerType.requiredInfo)"))
+            }
+        }
+        return .success(())
+    }
+}
